9291dff8ea7c1fb5699508a84d16fb399fab96c8
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-07 14:39:45 +0300

    first commit

---
+++ lesson4/biggest_number.rb
@@ -0,0 +1,16 @@
+a = 5
+b = 5
+puts a 
+puts b
+
+if a > b
+    puts "Наибольшее число:" + a.to_s
+end
+
+if a < b
+    puts "Наибольшее число:" + b.to_s
+end
+
+if b == a
+    puts "Оба числа равны"
+end
