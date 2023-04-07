5eb2e9a4cb9fe3d0281aa81d3dfc21f4c57424df
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-07 15:37:20 +0300

    six commit

---
+++ lesson4/coin2.rb
@@ -0,0 +1,9 @@
+if rand(11) == 10
+    puts "Монета встала на ребро"
+    else
+        if rand(2) == 1
+            puts "Выпала решка"
+        else
+            puts "Выпал орел"
+        end
+end
