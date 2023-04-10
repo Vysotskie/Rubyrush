0bac08762370dfbfadaf8f638ff2106e315bb96b
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-10 11:06:42 +0300

    third

---
+++ lesson5/mean.rb
@@ -0,0 +1,8 @@
+a = gets.to_i
+b = gets.to_i
+c = gets.to_i
+d = (a + b + c) / 3
+puts "Первое число: " + a.to_s
+puts "Второе число: " + b.to_s
+puts "Третье число: " + c.to_s
+puts "Среднее: " + d.to_s
