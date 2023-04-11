de18c6822e9a9f0f1f1e8f66cee46336a054cd7f
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-11 15:26:25 +0300

    third commit

---
+++ lesson6/invert.rb
@@ -0,0 +1,14 @@
+array = [1, 2, 3, 4, 5]
+
+puts "Исходный массив: "
+puts array.to_s
+
+puts "Массив в обратном порядке: "
+puts array.reverse.to_s
+
+puts "Исходный массив (не изменился: "
+puts array.to_s
+
+array.reverse!
+puts 'Исходный массив (после изменения):'
+puts array.to_s
