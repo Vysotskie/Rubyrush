a800479c92c88abd5c98761e3c87c7859a9d4988
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-11 12:17:52 +0300

    second commit

---
+++ lesson6/arrays.rb
@@ -0,0 +1,17 @@
+man = ["Миша" , "Гриша" , "Саша"]
+
+puts "Мужские имена: " 
+
+puts man.to_s
+
+woman = ["Настя" , "Катя" , "Маша"]
+
+puts "Женские имена: " 
+
+puts woman.to_s
+
+all = man + woman
+
+puts "Все имена: "
+
+puts all.to_s
