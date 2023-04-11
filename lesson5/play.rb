87a69ddde34ea2c50c6c7b3c4cb4a2d8d9cf0fa7
Anastasiya Vysotskaya <anastasiya_knizh@mail.ru>
2023-04-11 12:18:57 +0300

    first fix

--- lesson5/play.rb
+++ lesson5/play.rb
@@ -1,6 +1,6 @@
 number = rand(16)
 
-puts "Загадано число от 0 до 16, отгадайте какое?"
+puts "Загадано число от 0 до 15, отгадайте какое?"
 
 proba = gets.chomp.to_i
 
@@ -12,14 +12,8 @@ else
     else
         puts "Нужно больше"
     end
-end
 
-proba = gets.chomp.to_i
-
-if proba == number
-    abort "Ура, вы выиграли!"
-else 
-    if proba > number
+    if (proba - number).abs < 3
         puts "Тепло"
     else
         puts "Холодно"
@@ -32,6 +26,30 @@ if proba == number
     abort "Ура, вы выиграли!"
 else 
     if proba > number
+        puts "Нужно меньше"
+    else
+        puts "Нужно больше"
+    end
+
+    if (proba - number).abs < 3
+        puts "Тепло"
+    else
+        puts "Холодно"
+    end
+end
+
+proba = gets.chomp.to_i
+
+if proba == number
+    abort "Ура, вы выиграли!"
+else 
+    if proba > number
+        puts "Нужно меньше"
+    else
+        puts "Нужно больше"
+    end
+
+    if (proba - number).abs < 3
         puts "Тепло"
     else
         puts "Холодно"
